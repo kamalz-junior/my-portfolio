57faabbd2dcc0ac2b0b6747fcf071ea8000f8e18
kamalz-junior <kamalz.contacts@gmail.com>
2025-05-31 02:16:02 +0700

    fix: componenet

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Skill from "./components/skill";
+import Skill from "./components/Skill";
 import Work from "./components/Work";
 
 import { gsap } from "gsap";
